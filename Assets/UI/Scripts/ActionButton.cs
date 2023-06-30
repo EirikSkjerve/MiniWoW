3ab355a055129f74d9244e58c4870e35e141a080
Kaspar Moberg <kaspeti123@gmail.com>
2023-06-30 14:55:08 +0200

    added some documentation for ActionButton class

--- Assets/UI/Scripts/ActionButton.cs
+++ Assets/UI/Scripts/ActionButton.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+///<summary>
+/// Contains functionality of an Action button. This involes showing the appropritate name 
+/// and icon of a spell, aswell as graphically showing the remaining cooldown of the spell at the
+/// assigned action button.
+///</summary>
 public class ActionButton : MonoBehaviour
 {
     private SpellInfo _spellInfo;
@@ -17,12 +22,15 @@ public class ActionButton : MonoBehaviour
     [SerializeField] private Image coolDownFill;
     [SerializeField] private TMP_Text coolDownText;
 
-
-    public void UpdateCoolDown(float totalTime, float remainingTime) {
+    ///<summary>
+    /// Updates the cooldown elements of the action button given a remaining cooldown
+    ///</summary>
+    ///<param name="remainingTime">The remaining cooldown time of the spell</param>
+    public void UpdateCoolDown(float remainingTime) {
         coolDownText.gameObject.SetActive(true);
         coolDownFill.gameObject.SetActive(true);
 
-        coolDownFill.fillAmount = remainingTime / totalTime;
+        coolDownFill.fillAmount = remainingTime / SpellInfo.spellCooldown;
         coolDownText.text = Mathf.Round(remainingTime).ToString();
 
         if (remainingTime <= 0) {
@@ -31,7 +39,11 @@ public class ActionButton : MonoBehaviour
         }
     }
 
-
+    
+    ///<summary>
+    /// Sets the information given as a SpellInfo as the graphics for the action button. Updates the icon and name
+    ///</summary>
+    ///<param name="spellInfo">The SpellInfo object containing the info of the spell</param>
     private void UpdateActionButton(SpellInfo spellInfo) {
         GetComponent<Image>().sprite = spellInfo.spellIcon;
         GetComponentInChildren<TMP_Text>().text = spellInfo.spellName;
