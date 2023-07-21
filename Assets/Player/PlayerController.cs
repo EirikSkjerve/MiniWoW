6eff1b8e980fbe965502acdb57483b676ce29eca
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2023-07-21 10:49:48 +0200

    Targeting works..

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -6,12 +6,11 @@ using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour
 {
     public Player player;
-    
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -20,7 +19,6 @@ public class PlayerController : MonoBehaviour
         GetMovement();
         //player.SetCurrentTarget(GetClick());
         ListenForClicks();
-
     }
 
     private void GetMovement()
@@ -32,15 +30,16 @@ public class PlayerController : MonoBehaviour
         // a movement direction vector being a linear combination of basis vectors (0,1) (1,0) and the zero-vector (0,0)
         // Either: (0,0), (0,1), (1,0), (1,1), (0,-1), (-1,0), (-1,-1), (1,-1), or (-1,1)
         Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
-        
+
         //movement is smoothed out
         float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
         //vector is normalized
         movementDirection.Normalize();
 
         //changes the position of the player
-        player.transform.Translate(movementDirection * (player.GetRunSpeed() * inputMagnitude * Time.deltaTime), Space.World);
-        
+        player.transform.Translate(movementDirection * (player.GetRunSpeed() * inputMagnitude * Time.deltaTime),
+            Space.World);
+
         //updates the enum Direction of the player, given the calculated movementDirection vector.
         player.SetCurrentDirection(movementDirection);
     }
@@ -50,8 +49,6 @@ public class PlayerController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             TargetClickTarget();
-
-            
         }
     }
 
@@ -60,20 +57,17 @@ public class PlayerController : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-        if (hit.collider != null)
-        {
-            //Debug.Log(hit.collider.gameObject.ToString());
-            var objectType  = hit.collider.gameObject.GetType();
-            // Check if the clicked object is a valid target
-            if (objectType == typeof(Entity) && objectType != typeof(Player))
-            {
-                // Set the current target
-                player.SetCurrentTarget(hit.collider.gameObject);
+        if (hit.collider == null) return;
+        //Debug.Log(hit.collider.gameObject.ToString());
+        GameObject o;
+        var objectType = (o = hit.collider.gameObject).GetType();
+        // Check if the clicked object is a valid target
+        // Set the current target
+        Debug.Log(objectType.ToString());
+        player.SetCurrentTarget(o);
 
-                // Perform any additional actions or effects on the target
-                // For example, change the sprite color or play a sound
-                // ...
-            }
-        }
+        // Perform any additional actions or effects on the target
+        // For example, change the sprite color or play a sound
+        // ...
     }
 }
